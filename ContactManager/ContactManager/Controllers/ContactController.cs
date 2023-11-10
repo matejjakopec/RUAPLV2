4eab117f86aeb22f39984235b9c8e640e8507cca
HrvojeCosic <hrvojecosic222@gmail.com>
2023-11-10 18:02:41 +0100

    task 1 - simple read API

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Service;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
